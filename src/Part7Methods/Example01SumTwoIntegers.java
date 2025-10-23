4b6725dbace0cea56ef42b5af1df5f268156d44f
Giabao <giabao2344@gmail.com>
2025-10-23 21:29:02 +0700

    update example

---
+++ src/Part7Methods/Example01SumTwoIntegers.java
@@ -0,0 +1,11 @@
+package Part7Methods;
+
+public class Example01SumTwoIntegers {
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5, 3));
+    }
+}
